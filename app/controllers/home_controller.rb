7d219a27e6cfcc0e70dc0a129d32be6abc8bfc5d
James Simpson <jamesdanielsimpson@gmail.com>
2018-02-01 13:59:30 +0000

    Added multiple words, page reload function, and a title (temporary(

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,19 @@
 class HomeController < ApplicationController
   def index
-    random_phonic = ["b","d","e"] 
+    all_phonics = ('a'..'z').to_a
+    target_words = ["and", "bad", "bet", "big", "box", "can", "cat", "cup", 
+                    "cut", "dog", "get", "had", "has", "him", "his", "hot", 
+                    "let", "lot", "man", "not", "pet", "put", "red", "run", 
+                    "sit", "top", "yes", "yet"]
+    current_word = target_words.shuffle.first
+    random_phonics = all_phonics - current_word.split("")
     @item = {
-        target_word: "cat",
+        target_word: current_word,
         elements: {
-            0 => "c",
-            1 => "a",
-            2 => "t",
-            3 => random_phonic.shuffle.first
+            0 => current_word[0],
+            1 => current_word[1],
+            2 => current_word[2],
+            3 => random_phonics.shuffle.first
         }
     }
   end
